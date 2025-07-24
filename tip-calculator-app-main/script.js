bb6799350ebcb035e6e5ee2c8ef6feb3533e59a2
Avishek Sett <sett.avishek@gmail.com>
2025-07-24 17:31:53 -0400

    Added JS logic

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,156 @@
+const form = document.getElementById("form");
+const inputElements = document.querySelectorAll(".input-elements");
+const outputElements = document.querySelectorAll(".output-elements");
+
+const errorElements = form.querySelectorAll(".error-message");
+
+
+
+const tipRadioBtns = document.querySelectorAll(`input[type="radio"]`);
+const customTipValueEl = document.getElementById("custom-tip-value");
+
+
+function handleCustomTipChange(customTipEl, radioBtns, setName) {
+    if (customTipEl.value) {
+        customTipEl.name = setName;
+        for (const el of radioBtns) {
+            el.checked = false;
+        }
+    } else {
+        customTipEl.name = "";
+    }
+}
+
+function handleRadioBtnChange(radioBtn, customTipEl) {
+    if (radioBtn.checked) {
+        customTipEl.name = "";
+        customTipEl.value = "";
+    }
+}
+
+customTipValueEl.addEventListener("change", function() {
+    // Change the name to "tips" so that it is registered on the form
+    handleCustomTipChange(this, tipRadioBtns, "tips");
+
+})
+
+tipRadioBtns.forEach((btn) => {
+    btn.addEventListener("change", function()  {handleRadioBtnChange(this, customTipValueEl)});
+});
+
+
+function moreThanZero(val) {
+     if (val == 0 && val !== "") {
+        return "Can't be zero";
+    } else if (val < 0) {
+        return "Can't be less than zero";
+    }
+    return "";
+}
+
+function isInteger(val) {
+    return val - Math.trunc(val) === 0;
+}
+
+const validations = {
+    bill: (bill) => moreThanZero(bill),
+    tips: (tip) => {
+        if (tip < 0) {
+            return "Tip can't be negative";
+        }
+        return "";
+    },
+    "num-people": (numPeople) => {
+        let morethanzero = moreThanZero(numPeople);
+        if (morethanzero) {
+            return morethanzero;
+        } else if (!isInteger(numPeople)){
+            return "Can't have decimal";
+        } else {
+            return "";
+        }
+    }
+};
+
+const calculations = {
+    0: (dataObj) => {
+        return calculateTipPerPerson(dataObj["bill"], dataObj["tips"], dataObj["num-people"])
+    },
+    1: (dataObj) => {
+        return calculateTotalPerPerson(dataObj["bill"], dataObj["tips"], dataObj["num-people"])
+    }
+}
+
+
+
+function calculateTipPerPerson(bill, tipPercentage, numPeople) {
+
+    return numPeople != 0 ? (bill * (tipPercentage/100)) / numPeople: 0;
+}
+
+function calculateTotalPerPerson(bill, tipPercentage, numPeople) {
+    return numPeople != 0 ? (bill * (1 + tipPercentage/100)) / numPeople: 0;
+}
+
+function validKeyValue(key, value, validations) {
+    return !validations[key] || validations[key](value);
+}
+
+
+
+function validateForm(dataObj, validations) {
+    const errors = {};
+
+    for (const key of Object.keys(dataObj)) {
+        let error = validKeyValue(key, dataObj[key], validations);
+        errors[key] = error;
+    }
+
+    return errors;
+
+}
+
+
+
+function renderErrors(errors, errorElements, inputElements) {
+    let keys = Object.keys(errors);
+    let numErrors = 0;
+    for (let i=0; i<keys.length; i++) {
+        errorElements[i].textContent = errors[keys[i]];
+        if (errors[keys[i]]) {
+            inputElements[i].classList.add("input-error");
+            numErrors++;
+        } else {
+            inputElements[i].classList.remove("input-error");
+        }
+    }
+
+    return numErrors;
+}
+
+function updateOutput(outputElements, dataObj, calculations) {
+    for (let i=0; i<outputElements.length;i++) {
+        let output = calculations[i](dataObj);
+        // Round to two decimal places
+        outputElements[i].textContent = output.toFixed(2); 
+    }
+}
+
+function updateDOM(form, validations, calculations, errorElements, inputElements, outputElements) {
+    const formData = new FormData(form);
+    const dataObj = Object.fromEntries(formData);
+    const errors = validateForm(dataObj, validations); // Object with error messages
+
+    if (!renderErrors(errors, errorElements, inputElements)) { // Will return zero if all error messages were ""
+
+        updateOutput(outputElements, dataObj, calculations);
+        
+    }
+
+
+}
+
+form.addEventListener("change", (e) => {
+    e.preventDefault();
+    updateDOM(e.currentTarget, validations, calculations, errorElements, inputElements, outputElements);
+});
