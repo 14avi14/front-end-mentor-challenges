64e0fd30d79e05d8de3230509f0aeb233ce86a14
Avishek Sett <sett.avishek@gmail.com>
2025-07-25 21:26:33 -0400

    Added JS logic

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,41 @@
+const ratingStateEl = document.getElementById("rating-state");
+const form = document.getElementById("form");
+const errorEl = document.getElementById("input-error");
+const thankyouStateEl = document.getElementById("thank-you-state");
+const userRatingEl = document.getElementById("user-rating");
+
+function validateForm(data) {
+    let valid = true;
+    if (Object.keys(data).length === 0) {
+        valid = false;
+    }   
+    return valid;
+}
+
+function updateDisplay(data) {
+    ratingStateEl.classList.add("hidden");
+    thankyouStateEl.classList.remove("hidden");
+
+    userRatingEl.textContent = data["rating-btns"];
+}
+
+function handleSubmit(form, errorEl) {
+    const formData = new FormData(form);
+    const dataObj = Object.fromEntries(formData);
+
+    if (!validateForm(dataObj)) {
+        errorEl.textContent = "Please select a rating";
+        return;
+    } 
+
+    errorEl.textContent = "";
+
+    form.reset();
+    updateDisplay(dataObj);
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSubmit(e.target, errorEl);
+});
