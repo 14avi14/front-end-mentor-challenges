709666264d36d6772df31cebdf6f1dd29c510470
Avishek Sett <sett.avishek@gmail.com>
2025-07-28 16:10:39 -0400

    Added styles

--- contact-form-main/script.js
+++ contact-form-main/script.js
@@ -10,6 +10,12 @@ const form = document.getElementById("form");
 const errorElements = document.querySelectorAll(".errors");
 const toastMessage = document.getElementById("success-toast-message");
 
+// Make toast message disappear once animation is finished
+toastMessage.addEventListener("animationend", function() {
+    this.classList.remove("toast-message-slidedown-animation");
+    this.classList.add("hidden");
+})
+
 const validations = {
     "first-name": (fname) => !fname.trim() ? "This field is required": "", // Test for any non-space characters
     "last-name": (lname) => !lname.trim() ? "This field is required": "",
@@ -39,9 +45,13 @@ function validateForm(data, validations) {
     return errors;
 }
 
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 function handleSubmit(e, validations, errorElements) {
-    e.preventDefault()
+    e.preventDefault();    
 
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
@@ -51,22 +61,26 @@ function handleSubmit(e, validations, errorElements) {
     
     if (!Object.keys(errors).length) {
         e.target.reset();
-        toastMessage.style.visibility = "visible";
-    } else {
-        errorElements.forEach((element, index) => {
-            
-            if (index in errors) {
-                
-                errorElements[index].style.display = "block";
-                errorElements[index].textContent = errors[index];
-            } else {    
-                
-                errorElements[index].style.display = "none";
-                errorElements[index].textContent = "";
-            }
-        });
-        
+        toastMessage.classList.add("toast-message-slidedown-animation");
+        toastMessage.classList.remove("hidden");
+        goToTop();
+
     }
+
+    // Handle Error <span> elements
+    errorElements.forEach((element, index) => {
+        
+        if (index in errors) {
+            
+            errorElements[index].classList.remove("hidden");
+            errorElements[index].textContent = errors[index];
+        } else {    
+            
+            errorElements[index].classList.add("hidden");
+            errorElements[index].textContent = "";
+        }
+    });
+    
 }
 
 form.addEventListener("submit", (e) => {
