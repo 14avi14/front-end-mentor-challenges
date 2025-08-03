bdcd84c5d59dfce0bfe43226032b1e7e844e79c9
Avishek Sett <sett.avishek@gmail.com>
2025-08-02 21:27:04 -0400

    Added styles and logic

---
+++ room-homepage-master/script.js
@@ -0,0 +1,52 @@
+// Nav Bar 
+// Get Hamburger button 
+// When clicked, change class from open to close or close to open via toggle("btn--close")
+// Same toggle for nav(nav--open)
+const overlay = document.querySelector(".overlay");
+const openNavBtn = document.getElementById("openNavBtn");
+const navBar = document.querySelector(".nav__list");
+openNavBtn.addEventListener("click", (e) => {
+    overlay.classList.toggle("overlay--open");
+    openNavBtn.classList.toggle("openNavBtn--close"); // toggle "X" on the button
+    navBar.classList.toggle("nav__list--open");
+});
+
+// First, get all slide elements
+//      Images, and text sections
+//      Arrow Btns
+// Index variable
+// Event listener to change index
+// When index changes, add slideIn animation class to new indexed image, add slideOut animation class to old 
+
+const imagesSlideshow = document.querySelector(".imagesSlide");
+const textSlideshow = document.querySelector(".textSlide");
+const leftArrowBtn = document.getElementById("leftArrowBtn");
+const rightArrowBtn = document.getElementById("rightArrowBtn");
+let currentIndex = 0;
+const numSlides = 3;
+
+function handleIndexChange(index, direction, max) {
+    index += direction;
+    if (index >= max) {
+        index = 0;
+    } else if (index < 0) {
+        index = max - 1;
+    }
+    return index;
+}
+
+function handleSlideChange(direction) {
+    currentIndex = handleIndexChange(currentIndex, direction, numSlides);
+    imagesSlideshow.style.transform = `translateX(${-currentIndex*100}%)`;
+    textSlideshow.style.transform = `translateX(${-currentIndex*100/3}%)`;
+
+}
+
+
+leftArrowBtn.addEventListener("click", (e) => {
+    handleSlideChange(-1); // Remove from index
+});
+
+rightArrowBtn.addEventListener("click", (e) => {
+    handleSlideChange(1); // Add to index
+});
